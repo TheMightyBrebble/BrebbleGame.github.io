45b81a8b6c9acb0833ec8bc1dca84cc63a687bec
bradley <braddersc@aol.com>
2026-04-30 11:14:16 +0100

    Making quiz

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
 //Question Objects
 //Each object contains the question, the image for the question, 4 answers and then one of the answers printed as correctAnswer
 //That way we can compare correctAnswer and each answer to see which is correct. Means that when a button is pressed, the innerText can be compared to the question
-//Javascript is also designed in a way that adding another question and updating the switch/case also means
-//the quiz can technically go infinite.
-const question1 = {
-    question: "What is the capital city of Japan?",
-    question_img: "icons/question1.png",
-    answer1: "Osaka",
-    answer2: "Kyoto",
-    answer3: "Tokyo",
-    answer4: "Yokohama",
-    correctAnswer: "Tokyo",
-};
-const question2 = {
-    question:
-        "What type of energy drink was a sponsor in the game Death Stranding?",
-    question_img: "icons/heres-your-drink-norman-reedus.gif",
-    answer1: "Red Bull",
-    answer2: "Monster Energy",
-    answer3: "Lucozade",
-    answer4: "Rockstar",
-    correctAnswer: "Monster Energy",
-};
-const question3 = {
-    question: "What was the pirate name for Edward Teach?",
-    question_img: "icons/EdwardTeach.png",
-    answer1: "The Gentleman Pirate",
-    answer2: "Black Bart",
-    answer3: "Captain Kidd",
-    answer4: "Blackbeard",
-    correctAnswer: "Blackbeard",
-};
-const question4 = {
-    question: "When did the first Superman comic release?",
-    question_img: "icons/Superman.png",
-    answer1: "June 1938",
-    answer2: "July 1938",
-    answer3: "August 1938",
-    answer4: "April 1939",
-    correctAnswer: "June 1938",
-};
-const question5 = {
-    question: "When was the global release date for the PS5?",
-    question_img: "icons/PS5.png",
-    answer1: "Oct 29, 2020",
-    answer2: "Nov 12, 2020",
-    answer3: "Nov 19, 2020",
-    answer4: "Dec 10, 2020",
-    correctAnswer: "Nov 19, 2020",
-};
+//Javascript is also designed in a way that adding another question to the array and updating the switch/case also means
+//the quiz can technically go infinite if chosen to.
+const questions = [
+    {
+        question: "What is the capital city of Japan?",
+        question_img: "icons/question1.png",
+        answer1: "Osaka",
+        answer2: "Kyoto",
+        answer3: "Tokyo",
+        answer4: "Yokohama",
+        correctAnswer: "Tokyo",
+    },
+    {
+        question:
+            "What type of energy drink was a sponsor in the game Death Stranding?",
+        question_img: "icons/heres-your-drink-norman-reedus.gif",
+        answer1: "Red Bull",
+        answer2: "Monster Energy",
+        answer3: "Lucozade",
+        answer4: "Rockstar",
+        correctAnswer: "Monster Energy",
+    },
+    {
+        question: "What was the pirate name for Edward Teach?",
+        question_img: "icons/EdwardTeach.png",
+        answer1: "The Gentleman Pirate",
+        answer2: "Black Bart",
+        answer3: "Captain Kidd",
+        answer4: "Blackbeard",
+        correctAnswer: "Blackbeard",
+    },
+    {
+        question: "When did the first Superman comic release?",
+        question_img: "icons/Superman.png",
+        answer1: "June 1938",
+        answer2: "July 1938",
+        answer3: "August 1938",
+        answer4: "April 1939",
+        correctAnswer: "June 1938",
+    },
+    {
+        question: "When was the global release date for the PS5?",
+        question_img: "icons/PS5.png",
+        answer1: "Oct 29, 2020",
+        answer2: "Nov 12, 2020",
+        answer3: "Nov 19, 2020",
+        answer4: "Dec 10, 2020",
+        correctAnswer: "Nov 19, 2020",
+    },
+];
 
 //Necessary variables for the game to run
 const questionText = document.querySelector("#quiz_question-text");
@@ -60,19 +62,19 @@ const answer4 = document.querySelector("#quiz_answer4");
 const userScore = document.querySelector("#user_score");
 const totalScore = document.querySelector("#total_score");
 const timer = document.querySelector("#proceed_time");
-const timerBox = document.querySelector(".proceed_timer")
+const timerBox = document.querySelector(".proceed_timer");
 const nextQuest = document.querySelector("#proceed_nextQ");
 
 const amountOfTime = 20;
 let questionNumber = 1;
 let answerGiven = false;
-let question = question1;
-questionText.innerText = question1.question;
-questionImage.src = question1.question_img;
-answer1.innerText = question1.answer1;
-answer2.innerText = question1.answer2;
-answer3.innerText = question1.answer3;
-answer4.innerText = question1.answer4;
+let question = questions[0];
+questionText.innerText = questions[0].question;
+questionImage.src = questions[0].question_img;
+answer1.innerText = questions[0].answer1;
+answer2.innerText = questions[0].answer2;
+answer3.innerText = questions[0].answer3;
+answer4.innerText = questions[0].answer4;
 
 //Countdown Timer for game - only works using function keyword
 let timeLeft = amountOfTime;
@@ -120,7 +122,10 @@ answer4.addEventListener("click", (event) => {
 //Next Question button to go to the next question using Switch/Case
 nextQuest.addEventListener("click", (event) => {
     document.body.style.backgroundColor = "#ffcab1";
-    if (questionNumber <= 5) {
+    if (questionNumber < questions.length){
+        questionAdjustment(questions[questionNumber]);
+    }
+    if (questionNumber <= questions.length) {
         timeLeft = amountOfTime;
         totalScore.innerHTML = questionNumber;
         questionNumber++;
@@ -130,30 +135,21 @@ nextQuest.addEventListener("click", (event) => {
         }
         if (questionNumber > 5) {
             clearTimeout(timerId);
-            timer.innerText = "Your final score is: " + userScore.innerHTML + "/" + totalScore.innerHTML;
+            timer.innerText =
+                "Your final score is: " +
+                userScore.innerHTML +
+                "/" +
+                totalScore.innerHTML;
             const newElement = document.createElement("p");
-            const textNode = document.createTextNode("The button in the top left will reset the game");
+            const textNode = document.createTextNode(
+                "The button in the top left will reset the game",
+            );
             newElement.appendChild(textNode);
             timerBox.appendChild(newElement);
             timer.style.fontsize = "x-large";
             nextQuest.innerHTML = "The Game Is Over";
             //Potentially add another line about the home button resetting the game
         }
-    }
-
-    switch (questionNumber) {
-        case 2:
-            questionAdjustment(question2);
-            break;
-        case 3:
-            questionAdjustment(question3);
-            break;
-        case 4:
-            questionAdjustment(question4);
-            break;
-        case 5:
-            questionAdjustment(question5);
-            break;
     }
 });
 
